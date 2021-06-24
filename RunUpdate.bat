5a07623e1a2781a89014b743a34671e5c3d42a6e
Sawors <40705096+Sawors@users.noreply.github.com>
2021-06-24 22:50:47 +0200

    Update RunUpdate.bat

--- RunUpdate.bat
+++ RunUpdate.bat
@@ -5,6 +5,8 @@ git clone https://github.com/Sawors/1.16.5-Tiboise.git
 del ".\1.16.5-Tiboise\options.txt" /q
 del ".\1.16.5-Tiboise\optionsof.txt" /q
 xcopy ".\1.16.5-Tiboise\" "." /i /e /y
+del ".\1.16.5-Tiboise\" /f /q /s
+rd ".\1.16.5-Tiboise" /q /s
 echo:
 echo:
 echo  /================SAWORS=================\
