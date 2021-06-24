165801752394a47c71573b04bd38f34810acedf9
Sawors <40705096+Sawors@users.noreply.github.com>
2021-06-24 22:44:03 +0200

    Create RunUpdate.bat

---
+++ RunUpdate.bat
@@ -0,0 +1,19 @@
+@ECHO OFF
+del ".\1.16.5-Tiboise\" /f /q /s
+rd ".\1.16.5-Tiboise\" /q /s
+git clone https://github.com/Sawors/1.16.5-Tiboise.git
+del ".\1.16.5-Tiboise\options.txt" /q
+del ".\1.16.5-Tiboise\optionsof.txt" /q
+xcopy ".\1.16.5-Tiboise\" "." /i /e /y
+echo:
+echo:
+echo  /================SAWORS=================\
+echo  ^|                                       ^|
+echo  ^|                                       ^|
+echo  ^|           Modpack Updated !           ^|
+echo  ^|                                       ^|
+echo  ^|                                       ^|
+echo  \=======================================/
+echo:
+echo:
+pause
