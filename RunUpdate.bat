350008865bf46b04f64c251013284fcbec1d004a
Sawors <40705096+Sawors@users.noreply.github.com>
2021-06-27 23:23:06 +0200

    config_gen

--- RunUpdate.bat
+++ RunUpdate.bat
@@ -1,6 +1,7 @@
 @ECHO OFF
 del ".\1.16.5-Tiboise\" /f /q /s
 rd ".\1.16.5-Tiboise\" /q /s
+del ".\mods\" /f /q /s
 git clone https://github.com/Sawors/1.16.5-Tiboise.git
 del ".\1.16.5-Tiboise\options.txt" /q
 del ".\1.16.5-Tiboise\optionsof.txt" /q
