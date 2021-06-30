857307dc091cc67124d693680b955afde120d17b
Sawors <40705096+Sawors@users.noreply.github.com>
2021-06-30 14:09:17 +0200

    fd

--- RunUpdate.bat
+++ RunUpdate.bat
@@ -2,6 +2,7 @@
 del ".\1.16.5-Tiboise\" /f /q /s
 rd ".\1.16.5-Tiboise\" /q /s
 del ".\mods\" /f /q /s
+del ".\config\" /f /q /s
 git clone https://github.com/Sawors/1.16.5-Tiboise.git
 del ".\1.16.5-Tiboise\options.txt" /q
 del ".\1.16.5-Tiboise\optionsof.txt" /q
@@ -15,6 +16,7 @@ del ".\1.16.5-Tiboise\config\jei-client.toml" /f /q /s
 del ".\1.16.5-Tiboise\config\voicechat-client.toml" /f /q /s
 del ".\1.16.5-Tiboise\config\jei\" /f /q /s
 del ".\1.16.5-Tiboise\config\voicechat\" /f /q /s
+del ".\1.16.5-Tiboise\shaderpacks\" /f /q /s
 rem del ".\1.16.5-Tiboise\config\" /f /q /s
 rem }
 xcopy ".\1.16.5-Tiboise\" "." /i /e /y
